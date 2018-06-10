341865cdb4c565fa40f00e4f357ddbd8db5e271c
PierreCapo <pierre.caporossi@gadz.org>
2018-06-10 19:28:48 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: '/',
+    library: "Treeviz",
+    libraryTarget: "umd"
+  },
+  devServer: {
+    open: true, // open the browser automatically
+    historyApiFallback: true, // can use any URL and refresh the browser manually
+    contentBase: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    })
+  ],
+  node: {
+    net: 'empty',
+    tls: 'empty',
+    dns: 'empty'
+  }
+
+};
