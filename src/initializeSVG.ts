deddd49ccd08edf1b5fa7316bc19295f65c1f448
PierreCapo <pierre.caporossi@gadz.org>
2019-08-15 00:43:56 +0200

    :bug: Fix issue with wrong zoom behavior

--- src/initializeSVG.ts
+++ src/initializeSVG.ts
@@ -27,63 +27,36 @@ export const initiliazeSVG = (treeConfig: ITreeConfig) => {
   const width = areaWidth - margin.left - margin.right;
   const height = areaHeight - margin.top - margin.bottom;
 
-  const zoom = customD3
-    .zoom()
-    .scaleExtent([0.2, 20])
-    .on("zoom", () => {
-      svg.attr("transform", () => {
-        return mainAxisNodeSpacing === "auto"
-          ? "translate(" +
-              (margin.left + d3.event.transform.x) +
-              "," +
-              (margin.top + d3.event.transform.y) +
-              ")" +
-              "scale(" +
-              d3.event.transform.k +
-              ")"
-          : isHorizontal
-          ? "translate(" +
-            (margin.left + d3.event.transform.x) +
-            "," +
-            (margin.top + height / 2 - nodeHeight / 2 + d3.event.transform.y) +
-            ")" +
-            "scale(" +
-            d3.event.transform.k +
-            ")"
-          : "translate(" +
-            (margin.left + width / 2 - nodeWidth / 2 + d3.event.transform.x) +
-            "," +
-            (margin.top + d3.event.transform.y) +
-            ")" +
-            "scale(" +
-            d3.event.transform.k +
-            ")";
-      });
-    });
-
   const svg = d3
     .select("#" + htmlId)
     .append("svg")
     .attr("width", areaWidth)
     .attr("height", areaHeight)
-    // @ts-ignore
-    .call(hasPanAndZoom ? zoom : () => this)
-    .append("g")
-    .attr(
-      "transform",
-      mainAxisNodeSpacing === "auto"
-        ? "translate(0,0)"
-        : isHorizontal
-        ? "translate(" +
-          margin.left +
-          "," +
-          (margin.top + height / 2 - nodeHeight / 2) +
-          ")"
-        : "translate(" +
-          (margin.left + width / 2 - nodeWidth / 2) +
-          "," +
-          margin.top +
-          ")"
+    .call(
+      // @ts-ignore
+      customD3
+        .zoom()
+        .on("zoom", () =>
+          hasPanAndZoom ? ZoomG.attr("transform", d3.event.transform) : null
+        )
     );
-  return svg;
+
+  const ZoomG = svg.append("g");
+  const MainG = ZoomG.append("g").attr(
+    "transform",
+    mainAxisNodeSpacing === "auto"
+      ? "translate(0,0)"
+      : isHorizontal
+      ? "translate(" +
+        margin.left +
+        "," +
+        (margin.top + height / 2 - nodeHeight / 2) +
+        ")"
+      : "translate(" +
+        (margin.left + width / 2 - nodeWidth / 2) +
+        "," +
+        margin.top +
+        ")"
+  );
+  return MainG;
 };
