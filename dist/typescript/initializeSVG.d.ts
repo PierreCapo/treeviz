41d883cfa16e377c8cf2d3a8e2c74733f7d487ed
PierreCapo <pierre.caporossi@gadz.org>
2020-04-07 00:24:59 +0200

    :config: refactor using lerna and tsdx

--- dist/typescript/initializeSVG.d.ts
+++
@@ -1,3 +0,0 @@
-import * as d3 from "d3-selection";
-import { ITreeConfig } from "./typings";
-export declare const initiliazeSVG: (treeConfig: ITreeConfig) => d3.Selection<SVGGElement, unknown, HTMLElement, any>;
--- dist/typescript/nodes/node-update.d.ts
+++
@@ -1,3 +0,0 @@
-import { BaseType, Selection } from "d3-selection";
-import { ExtendedHierarchyPointNode, ITreeConfig } from "../typings";
-export declare const drawNodeUpdate: (nodeEnter: Selection<SVGGElement, ExtendedHierarchyPointNode, SVGGElement, {}>, node: Selection<BaseType, ExtendedHierarchyPointNode, SVGGElement, {}>, settings: ITreeConfig) => void;
